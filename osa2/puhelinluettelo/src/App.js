9f4be456c606d1bd7c42ecf7f5a227de3e73d102
Walter Grönholm <waltergronholm@gmail.com>
2020-02-03 14:36:02 +0200

    osa2 2.20 skipattu

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import ds from './DataService.js'
+import './App.css'
 
 const Filter = ({newFilter, handler}) => (
     <div>
@@ -32,18 +33,34 @@ const Numbers = ({persons, newFilter}) => (
   </div>
 )
 
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className="error">
+      {message}
+    </div>
+  )
+}
+
+const ALERT_TIMEOUT = 3000
+
 const App = () => {
   const [ persons, setPersons ] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
-      axios
-        .get('http://localhost:3001/persons')
-        .then(response => {
-          console.log('loaded data')
-          setPersons(response.data)
-        })
+      ds.getAll().then(setPersons).catch(error => {
+        setErrorMessage(
+          `Couldn't contact the database server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, ALERT_TIMEOUT)})
     }, [])
- 
+
   const [ newName, setNewName ] = useState('')
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -62,27 +79,61 @@ const App = () => {
 
   const addEntry = (event) => {
     event.preventDefault()
-    for (const person of persons) {
-      if (person.name === newName) {
-        alert(`${newName} is already added to phonebook`)
-        return
-      }
-    }
-    const entryObject = {
-      name: newName,
-      number: newNumber
+
+    if (newName.length === 0) {
+      setErrorMessage("Please add a name")
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, ALERT_TIMEOUT)
+      return
     }
 
-    setPersons(persons.concat(entryObject))
+    const filtered = persons.filter((person) => person.name === newName)
+
+    const entryObject = filtered.length === 0 ? {
+      name: newName,
+      number: newNumber
+    } : { ...filtered[0], number: newNumber }
+
+    if (entryObject.id) {
+      if (!newNumber) {
+        if (window.confirm("Delete " + entryObject.name + "'s entry?")) {
+          ds.remove(entryObject.id).catch(error => {
+            setErrorMessage(
+              `${entryObject.name}' was already removed from server`
+            )
+            setTimeout(() => {
+              setErrorMessage(null)
+            }, ALERT_TIMEOUT)})
+          setPersons(persons.filter(person => person.id !== entryObject.id))
+        } else return
+      } else if (window.confirm("Replace " + entryObject.name + "'s old number?")) {
+        ds.update(entryObject.id, entryObject)
+        setPersons(persons.filter(person => person.id !== entryObject.id).concat(entryObject))
+      } else return
+    } else {
+      if (!newNumber) {
+        setErrorMessage("Couldn't find an entry for " + newName + " to delete")
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, ALERT_TIMEOUT)
+      } else {
+        ds.create(entryObject)
+        setPersons(persons.concat(entryObject))
+      }
+    }
+
     setNewName('')
     setNewNumber('')
   }
   
   return (
     <div>
+      <Notification message={errorMessage} />
       <h2>Phonebook</h2>
       <Filter newFilter={newFilter} handler={handleFilterChange} />
-      <h2>Add a new entry</h2>
+      <h2>Add/Change a new entry</h2>
+      <p>Delete an entry by leaving the number field blank</p>
       <Addition newName={newName} newNumber={newNumber}
                 nameHandler={handleNameChange}
                 numberHandler={handleNumberChange}
