8ddb2b935eb37d22d89c3d3905dd7e33baf5c7bb
Walter Grönholm <waltergronholm@gmail.com>
2020-02-10 18:04:37 +0200

    uibuild

--- osa2/puhelinluettelo/src/DataService.js
+++ osa2/puhelinluettelo/src/DataService.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = 'https://boiling-forest-02952.herokuapp.com/api/persons'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
