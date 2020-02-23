3f455d267539abe431706fe9a9bda3d965a68f20
Walter Grönholm <waltergronholm@gmail.com>
2020-02-24 00:40:12 +0200

    osa5a

---
+++ osa5/bloglist-frontend/src/App.js
@@ -0,0 +1,175 @@
+import React, { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const Notification = ({ message }) => {
+
+	if (message === null) {
+		return null
+	}
+
+	return (
+		<div className="error">
+			{message}
+		</div>
+	)
+}
+
+const ALERT_TIMEOUT = 5000
+
+const App = () => {
+	const [blogs, setBlogs] = useState([])
+	const [username, setUsername] = useState('') 
+	const [password, setPassword] = useState('')
+	const [user, setUser] = useState(null)
+
+	const [title, setTitle] = useState('') 
+	const [author, setAuthor] = useState('')
+	const [url, setUrl] = useState('')
+	const [errorMessage, setErrorMessage] = useState(null)
+
+	useEffect(() => {
+		blogService.getAll().then(blogs =>
+			setBlogs( blogs )
+		)	
+	}, [])
+
+	useEffect(() => {
+		const currentUserJSON = window.localStorage.getItem('currentUser')
+		if (currentUserJSON) {
+			const user = JSON.parse(currentUserJSON)
+			setUser(user)
+			blogService.setToken(user.token)
+		}
+	  }, [])
+
+	const handleLogin = async (event) => {
+		event.preventDefault()
+		console.log("Logging in to " + username)
+		try {
+			const user = await loginService.login({
+				login: username, password,
+			})
+		
+			window.localStorage.setItem(
+				'currentUser', JSON.stringify(user)
+			) 
+			setUser(user)
+			blogService.setToken(user.token)
+			setUsername('')
+			setPassword('')
+		} catch (exception) {
+			console.log("Nope: " + exception)
+			setErrorMessage(
+				"Username or password wrong"
+			)
+			setTimeout(() => {
+				setErrorMessage(null)
+			}, ALERT_TIMEOUT)
+		}
+	}
+
+	const handleLogout = (event) => {
+		event.preventDefault()
+		window.localStorage.removeItem('currentUser')
+		setUser(null)
+		blogService.setToken("")
+		setUsername('')
+		setPassword('')
+	}
+
+	if (user === null) {
+		return (
+		<div>
+			<Notification message={errorMessage} />
+			<h2>Log in to application</h2>
+			<form onSubmit={handleLogin}>
+				<div>
+					username
+						<input
+						type="text"
+						value={username}
+						name="Username"
+						onChange={({ target }) => setUsername(target.value)}
+					/>
+				</div>
+				<div>
+					password
+						<input
+						type="password"
+						value={password}
+						name="Password"
+						onChange={({ target }) => setPassword(target.value)}
+					/>
+				</div>
+				<button type="submit">login</button>
+			</form>
+		</div>
+		)
+	}
+
+	const handleAddition = async (event) => {
+		event.preventDefault()
+		console.log("Adding " + title)
+		try {
+			const blog = await blogService.create({
+				title, author, url,
+			})
+
+			setBlogs(blogs.concat([blog]))
+			setErrorMessage(
+				"Added blog " + title
+			)
+			setTimeout(() => {
+				setErrorMessage(null)
+			}, ALERT_TIMEOUT)
+		} catch (exception) {
+			console.log("Nope: " + exception)
+		}
+	}
+
+	return (
+		<div>
+			<Notification message={errorMessage} />
+			<h2>{user.name} is logged in</h2><button onClick={handleLogout}>Log out</button>
+			<h2>Create new blog</h2>
+			<form onSubmit={handleAddition}>
+				<div>
+					title
+						<input
+						type="text"
+						value={title}
+						name="Title"
+						onChange={({ target }) => setTitle(target.value)}
+					/>
+				</div>
+				<div>
+					author
+						<input
+						type="text"
+						value={author}
+						name="Author"
+						onChange={({ target }) => setAuthor(target.value)}
+					/>
+				</div>
+				<div>
+					url
+						<input
+						type="url"
+						value={url}
+						name="Url"
+						onChange={({ target }) => setUrl(target.value)}
+					/>
+				</div>
+				<button type="submit">Submit</button>
+			</form>
+			<h2>blogs</h2>
+			{blogs.map(blog =>
+				<Blog key={blog.id} blog={blog} />
+			)}
+		</div>
+	)
+}
+
+export default App
