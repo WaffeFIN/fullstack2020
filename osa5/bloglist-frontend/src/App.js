68bfc223e10cf84e73585a0df437c7063d0d68c5
Walter Grönholm <waltergronholm@gmail.com>
2020-02-24 13:29:33 +0200

    osa5b

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -1,7 +1,9 @@
-import React, { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
-import loginService from './services/login'
+import React, { useState, useEffect, useImperativeHandle } from "react"
+import PropTypes from "prop-types"
+
+import Blog from "./components/Blog"
+import blogService from "./services/blogs"
+import loginService from "./services/login"
 
 const Notification = ({ message }) => {
 
@@ -16,33 +18,75 @@ const Notification = ({ message }) => {
 	)
 }
 
+const Togglable = React.forwardRef((props, ref) => {
+	const [visible, setVisible] = useState(false)
+
+	const hideWhenVisible = { display: visible ^ props.inverted ? "none" : "" }
+	const showWhenVisible = { display: visible ^ props.inverted ? "" : "none" }
+
+	const toggleVisibility = () => {
+		setVisible(!visible)
+	}
+
+	useImperativeHandle(ref, () => {
+		return {
+			toggleVisibility
+		}
+	})
+
+	return (
+		<div>
+			<div style={hideWhenVisible}>
+				<button onClick={toggleVisibility}>{props.buttonLabel}</button>
+			</div>
+			<div style={showWhenVisible}>
+				{props.children}
+				<button onClick={toggleVisibility}>cancel</button>
+			</div>
+		</div>
+	)
+})
+
+Togglable.propTypes = {
+	buttonLabel: PropTypes.string.isRequired
+}
+Togglable.displayName = "Togglable"
+
+/*
+const noteFormRef = React.createRef()
+
+  <Togglable buttonLabel="new note" ref={noteFormRef}>//*/
+
 const ALERT_TIMEOUT = 5000
 
 const App = () => {
 	const [blogs, setBlogs] = useState([])
-	const [username, setUsername] = useState('') 
-	const [password, setPassword] = useState('')
+	const [username, setUsername] = useState("") 
+	const [password, setPassword] = useState("")
 	const [user, setUser] = useState(null)
 
-	const [title, setTitle] = useState('') 
-	const [author, setAuthor] = useState('')
-	const [url, setUrl] = useState('')
+	const [title, setTitle] = useState("") 
+	const [author, setAuthor] = useState("")
+	const [url, setUrl] = useState("")
 	const [errorMessage, setErrorMessage] = useState(null)
+
+	const loginRef = React.createRef() 
+	const newBlogRef = React.createRef() 
 
 	useEffect(() => {
 		blogService.getAll().then(blogs =>
 			setBlogs( blogs )
-		)	
+		)
 	}, [])
 
 	useEffect(() => {
-		const currentUserJSON = window.localStorage.getItem('currentUser')
+		const currentUserJSON = window.localStorage.getItem("currentUser")
 		if (currentUserJSON) {
 			const user = JSON.parse(currentUserJSON)
 			setUser(user)
 			blogService.setToken(user.token)
 		}
-	  }, [])
+	}, [])
 
 	const handleLogin = async (event) => {
 		event.preventDefault()
@@ -51,14 +95,16 @@ const App = () => {
 			const user = await loginService.login({
 				login: username, password,
 			})
-		
+
 			window.localStorage.setItem(
-				'currentUser', JSON.stringify(user)
+				"currentUser", JSON.stringify(user)
 			) 
 			setUser(user)
 			blogService.setToken(user.token)
-			setUsername('')
-			setPassword('')
+			setUsername("")
+			setPassword("")
+			if (loginRef.current)
+				loginRef.current.toggleVisibility()
 		} catch (exception) {
 			console.log("Nope: " + exception)
 			setErrorMessage(
@@ -72,41 +118,13 @@ const App = () => {
 
 	const handleLogout = (event) => {
 		event.preventDefault()
-		window.localStorage.removeItem('currentUser')
+		window.localStorage.removeItem("currentUser")
 		setUser(null)
 		blogService.setToken("")
-		setUsername('')
-		setPassword('')
-	}
-
-	if (user === null) {
-		return (
-		<div>
-			<Notification message={errorMessage} />
-			<h2>Log in to application</h2>
-			<form onSubmit={handleLogin}>
-				<div>
-					username
-						<input
-						type="text"
-						value={username}
-						name="Username"
-						onChange={({ target }) => setUsername(target.value)}
-					/>
-				</div>
-				<div>
-					password
-						<input
-						type="password"
-						value={password}
-						name="Password"
-						onChange={({ target }) => setPassword(target.value)}
-					/>
-				</div>
-				<button type="submit">login</button>
-			</form>
-		</div>
-		)
+		setUsername("")
+		setPassword("")
+		if (loginRef.current)
+			loginRef.current.toggleVisibility()
 	}
 
 	const handleAddition = async (event) => {
@@ -132,12 +150,34 @@ const App = () => {
 	return (
 		<div>
 			<Notification message={errorMessage} />
-			<h2>{user.name} is logged in</h2><button onClick={handleLogout}>Log out</button>
+			{ user ? [LoggedIn({
+				user,
+				handleLogout
+			}), <Togglable buttonLabel="Create new blog" ref={newBlogRef}>
+				{NewBlog({
+					handleAddition,
+					title, setTitle,
+					author, setAuthor,
+					url, setUrl
+				})}
+			</Togglable>] : Login({
+				handleLogin,
+				username, setUsername,
+				password, setPassword
+			})}
+			<BlogList blogs={blogs} />
+		</div>
+	)
+}
+
+const NewBlog = ( { handleAddition, title, setTitle, author, setAuthor, url, setUrl} ) => {
+	return (
+		<div>
 			<h2>Create new blog</h2>
 			<form onSubmit={handleAddition}>
 				<div>
 					title
-						<input
+					<input
 						type="text"
 						value={title}
 						name="Title"
@@ -146,7 +186,7 @@ const App = () => {
 				</div>
 				<div>
 					author
-						<input
+					<input
 						type="text"
 						value={author}
 						name="Author"
@@ -155,7 +195,7 @@ const App = () => {
 				</div>
 				<div>
 					url
-						<input
+					<input
 						type="url"
 						value={url}
 						name="Url"
@@ -164,12 +204,66 @@ const App = () => {
 				</div>
 				<button type="submit">Submit</button>
 			</form>
-			<h2>blogs</h2>
+		</div>
+	)
+}
+
+NewBlog.propTypes = {
+	handleSubmit: PropTypes.func.isRequired,
+	handleUsernameChange: PropTypes.func.isRequired,
+	handlePasswordChange: PropTypes.func.isRequired,
+	username: PropTypes.string.isRequired,
+	password: PropTypes.string.isRequired
+}
+
+const LoggedIn = ( { user, handleLogout } ) => {
+	if (user !== null)
+		return (
+			<div>
+				<h2>{user.name} is logged in</h2>
+				<button onClick={handleLogout}>Log out</button>
+			</div>
+		)
+}
+
+const Login = ( { handleLogin, username, setUsername, password, setPassword } ) => {
+	return (
+		<div>
+			<h2>Log in to application</h2>
+			<form onSubmit={handleLogin}>
+				<div>
+					username
+					<input
+						type="text"
+						value={username}
+						name="Username"
+						onChange={({ target }) => setUsername(target.value)}
+					/>
+				</div>
+				<div>
+					password
+					<input
+						type="password"
+						value={password}
+						name="Password"
+						onChange={({ target }) => setPassword(target.value)}
+					/>
+				</div>
+				<button type="submit">login</button>
+			</form>
+		</div>
+	)
+}
+
+const BlogList = ({ blogs }) => {
+	blogs.sort((a, b) => a.likes > b.likes)
+	return (
+		<div>
+			<h2>Blogs</h2>
 			{blogs.map(blog =>
 				<Blog key={blog.id} blog={blog} />
 			)}
 		</div>
 	)
 }
-
 export default App
